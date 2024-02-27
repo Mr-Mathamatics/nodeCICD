99bc91c22538d5005e426765984a47f8bae25e39
alkeshrails <akleshrailsdev@gmail.com>
2024-02-27 15:00:51 +0530

    add index.html

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const path = require("path");
 const app = express();
 
 app.get('/',(req,res)=>{
-	res.send("hello Api")
+	res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(8000,()=>console.log('server connected at 8000 port'));
